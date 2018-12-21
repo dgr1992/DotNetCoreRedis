253e3581a02e3197ca0c85693b54827ffa93d429
daniel <daniel@familie-gross.at>
2018-12-21 16:38:09 +0100

    Receiver

---
+++ RedisMessanger/Receiver/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RedisManagerPool redisManager = new RedisManagerPool("localhost:63790"))
+            {
+                using (IRedisClient redisClient = redisManager.GetClient())
+                {
+                    try
+                    {
+                        Console.WriteLine("#### Listening to channel 'Chat' ####");
+                        string msg = "";
+                        while(true)
+                        {
+                            msg = redisClient.BlockingRemoveStartFromList("Chat", new TimeSpan(0));
+                            Console.WriteLine(msg);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+        }
+    }
+}
