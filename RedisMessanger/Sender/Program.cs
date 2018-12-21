434544d09ef2db6e98a3788cb4469b2c0b323d15
daniel <daniel@familie-gross.at>
2018-12-21 16:37:27 +0100

    Message sender

---
+++ RedisMessanger/Sender/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ServiceStack.Redis;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (RedisManagerPool redisManager = new RedisManagerPool("localhost:63790"))
+            {
+                using (IRedisClient redisClient = redisManager.GetClient())
+                {
+                    try
+                    {
+                        Console.WriteLine("#### Sending to channel 'Chat' ####");
+                        string msg = "";
+                        while(true)
+                        {   
+                            Console.Write("Message to 'Chat': ");
+                            msg = Console.ReadLine();
+                            redisClient.AddItemToList("Chat", msg);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+        }
+    }
+}
